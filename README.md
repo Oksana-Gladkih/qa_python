1cd45f683cd1699bc6ea68d9925672a7630e6c6a
oksana gladkih <s741to@yandex.ru>
2023-12-18 10:17:03 +0300

    Add tests

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # qa_python
+
+В проекте реализованы тесты:
+1. test_add_new_book_add_two_books - Проверяется добавление книги и двух книг
+2. test_add_new_book_from_title_and_added - Проверяется добавление определеннной книги в список
+3. test_get_books_for_children_with_invalid_genre - Проверяется добавление для детей с неверным жанром
+4. test_get_book_genre  - Проверяется добавление книги с одним жанром
+5. test_get_books_with_specific_genre- Проверяется добавление книги с определенным жанром
+6. test_get_books_genre - Проверяется добавление разного жанра
+7. test_add_book_in_favorites - Проверяется добавление книги из любимого жанра
+8. test_delete_book_from_favorites - Проверяется удаление книги из избранного 
+9. test_add_new_book_with_invalid_name -Проверяется поиск по названию которого нет 
+10. test_set_invalid_book_genre - Проверяется ввод неверного названия книги
+11. test_get_books_with_invalid_genre - Проверяется поиск по неверному названию жанра
+
--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -18,7 +20,65 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    @pytest.mark.parametrize('name', ['Террариум', 'Лолита', 'Книзь Юсупов'])
+    def test_add_new_book_from_title_and_added(self, name):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        assert name in collector.books_genre
+
+    def test_get_books_for_children_with_invalid_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Книга для взрослых")
+        collector.set_book_genre("Книга для взрослых", "Недетский жанр")
+        children_books = collector.get_books_for_children()
+        assert "Книга для взрослых" not in children_books
+
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Марсианин")
+        collector.set_book_genre("Марсианин", "Фантастика")
+        assert collector.get_book_genre("Марсианин") == "Фантастика"
+
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Чужие")
+        collector.set_book_genre("Чужие", "Ужасы")
+        assert "Чужие" in collector.get_books_with_specific_genre("Ужасы")
+
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book("Матрица")
+        collector.set_book_genre("Матрица", "Фантастика")
+        assert "Матрица" in collector.get_books_genre()
+
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гарри Поттер и Философский камень")
+        collector.add_book_in_favorites("Гарри Поттер и Философский камень")
+        assert "Гарри Поттер и Философский камень" in collector.get_list_of_favorites_books()
+
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+        collector.add_new_book("Гарри Поттер и Кубок огня")
+        collector.add_book_in_favorites("Гарри Поттер и Кубок огня")
+        collector.delete_book_from_favorites("Гарри Поттер и Кубок огня")
+        assert "Гарри Поттер и Кубок огня" not in collector.get_list_of_favorites_books()
+
+    def test_add_new_book_with_invalid_name(self):
+        collector = BooksCollector()
+        collector.add_new_book("Анна Каренина, очень длинное название для книги, которое превышает 40 символов")
+        assert "Анна Каренина, очень длинное название для книги, которое превышает 40 символов" not in collector.books_genre
+
+    def test_set_invalid_book_genre(self):
+        collector = BooksCollector()
+        collector.set_book_genre("Террариум", "Неизвестный Жанр")
+        assert collector.get_book_genre("Террариум") is None
+
+    def test_get_books_with_invalid_genre(self):
+        collector = BooksCollector()
+        assert len(collector.get_books_with_specific_genre("Неизвестный Жанр")) == 0
